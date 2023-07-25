5b45032bf8dc55772fdf87751bec0d2f073388af
Jonathan Montecinos <montecinosjony@gmail.com>
2023-07-25 00:51:20 -0300

    Creado el array y como el usuario puede generar el input

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-class Producto {
+class Disco {
     constructor(artista, album, anio, escuchado){
         this.artista = artista;
         this.album = album;
@@ -7,4 +7,16 @@ class Producto {
     }
 }
 
-    
+var discosArray = [];
+
+
+do {
+    var art = prompt("Ingrese Artista o Ingrese 'exit' para finalizar");
+    if (art.toLowerCase() === 'exit') {
+        break;
+    }
+    var alb = prompt("Ingrese Nombre de Album");
+    var an = prompt("Ingrese Año del Album");
+    var esc = prompt("Ingrese si ha escuchado el Album");
+    discosArray.push(new Disco(art,alb,an,esc));
+}  while (true);
