4233bed0ffde8eff21e4b6b37117687641cc4dbc
Jonathan Montecinos <montecinosjony@gmail.com>
2023-07-25 00:08:05 -0300

    Primer comit con archvivos creados, inicializando la clasee

---
+++ script.js
@@ -0,0 +1,10 @@
+class Producto {
+    constructor(artista, album, anio, escuchado){
+        this.artista = artista;
+        this.album = album;
+        this.anio = anio;
+        this.escuchado = escuchado;
+    }
+}
+
+    
