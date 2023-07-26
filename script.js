6f39fbe2ef19792dd5fcbab32116577ad904ad02
Jonathan Montecinos <montecinosjony@gmail.com>
2023-07-25 23:03:13 -0300

    Agregue la info via form de html al array en javascript

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// Declare la info inicial de la clase disco que requiere un artista album anio y escuchado
 class Disco {
     constructor(artista, album, anio, escuchado){
         this.artista = artista;
@@ -7,10 +9,12 @@ class Disco {
     }
 }
 
+
 var discosArray = [];
 
 
-do {
+// Cargar el array con Bublce Do While (Funcionando)
+/*do {
     var art = prompt("Ingrese Artista o Ingrese 'exit' para finalizar");
     if (art.toLowerCase() === 'exit') {
         break;
@@ -20,3 +24,28 @@ do {
     var esc = prompt("Ingrese si ha escuchado el Album");
     discosArray.push(new Disco(art,alb,an,esc));
 }  while (true);
+*/
+
+// Recibir la informacion del Form en html
+document.getElementById('dataForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent form submission to avoid page reload
+  
+    // Obtengo los Valores con get element by id y el valor 
+    const art = document.getElementById('Artist').value;
+    const alb = document.getElementById('Album').value;
+    const an = document.getElementById('Anio').value;
+    const esc = document.getElementById('Escuchado').value;
+
+    // Agrego la info al array
+    discosArray.push(new Disco(art,alb,an,esc));
+  
+    // Muestro el array
+    console.log(discosArray);
+  
+    // Limpio el Form en html
+    document.getElementById('Artist').value = '';
+    document.getElementById('Album').value = '';
+    document.getElementById('Anio').value = '';
+    document.getElementById('Escuchado').value = '';
+  });
+  
