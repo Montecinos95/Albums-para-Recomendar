12c704847ef07c6b95ae408d712119c835481190
Jonathan Montecinos <montecinosjony@gmail.com>
2023-08-14 21:54:58 -0300

    Aplicacion Lista

--- script.js
+++ script.js
@@ -1,51 +1,81 @@
-
-// Declare la info inicial de la clase disco que requiere un artista album anio y escuchado
 class Disco {
-    constructor(artista, album, anio, escuchado){
+    constructor(artista, album, anio) {
         this.artista = artista;
         this.album = album;
         this.anio = anio;
-        this.escuchado = escuchado;
     }
 }
 
+let discosArray = [];
 
-var discosArray = [];
+// Load items from localStorage if available
+const storedItems = localStorage.getItem("discosArray");
+if (storedItems) {
+    discosArray = JSON.parse(storedItems);
+}
 
-
-// Cargar el array con Bublce Do While (Funcionando)
-/*do {
-    var art = prompt("Ingrese Artista o Ingrese 'exit' para finalizar");
-    if (art.toLowerCase() === 'exit') {
-        break;
-    }
-    var alb = prompt("Ingrese Nombre de Album");
-    var an = prompt("Ingrese Año del Album");
-    var esc = prompt("Ingrese si ha escuchado el Album");
-    discosArray.push(new Disco(art,alb,an,esc));
-}  while (true);
-*/
-
-// Recibir la informacion del Form en html
 document.getElementById('dataForm').addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent form submission to avoid page reload
-  
-    // Obtengo los Valores con get element by id y el valor 
+    event.preventDefault();
+
     const art = document.getElementById('Artist').value;
     const alb = document.getElementById('Album').value;
     const an = document.getElementById('Anio').value;
-    const esc = document.getElementById('Escuchado').value;
 
-    // Agrego la info al array
-    discosArray.push(new Disco(art,alb,an,esc));
-  
-    // Muestro el array
+
+    discosArray.push(new Disco(art, alb, an));
+
+    // Save updated array to localStorage
+    localStorage.setItem("discosArray", JSON.stringify(discosArray));
+
     console.log(discosArray);
-  
-    // Limpio el Form en html
+
     document.getElementById('Artist').value = '';
     document.getElementById('Album').value = '';
     document.getElementById('Anio').value = '';
-    document.getElementById('Escuchado').value = '';
-  });
-  
+
+
+    updateArrayDisplay(); // Update and display the array
+});
+
+const arrayDisplayElement = document.getElementById("arrayDisplay");
+const ul = document.createElement("ul");
+arrayDisplayElement.appendChild(ul);
+
+function updateArrayDisplay() {
+    ul.innerHTML = ''; // Clear the existing list
+
+    // Loop through the array and create list items
+    discosArray.forEach((disco, index) => {
+        const li = document.createElement("li");
+        li.textContent = `Artista: ${disco.artista}, Album: ${disco.album}, Año: ${disco.anio}`;
+    
+        const botonBuscar = document.createElement("button");
+        botonBuscar.textContent = "Buscar Youtube";
+        botonBuscar.addEventListener("click", () => {
+            let encodedQuery = encodeURIComponent(disco.artista + ' ' + disco.album + ' ' + disco.anio);
+            var newPageUrl = "https://www.youtube.com/results?search_query=" + encodedQuery;
+    
+            // Open the new page in a new tab/window
+            window.open(newPageUrl, "_blank");
+        });
+    
+        const deleteButton = document.createElement("button"); // Create delete button
+        deleteButton.textContent = "Delete"; // Button text
+        deleteButton.addEventListener("click", () => {
+            // Remove the disco from the array
+            discosArray.splice(index, 1);
+            // Save updated array to localStorage
+            localStorage.setItem("discosArray", JSON.stringify(discosArray));
+            // Update the display
+            updateArrayDisplay();
+        });
+    
+        li.appendChild(botonBuscar);
+        li.appendChild(deleteButton); // Append delete button
+    
+        ul.appendChild(li);
+    });
+}
+
+// Initial display
+updateArrayDisplay();
